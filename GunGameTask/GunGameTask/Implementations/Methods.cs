c5fd392c27e14d820a29756b88adcc210738e64d
Umid Museyibli <118839456+Elfrustaichi@users.noreply.github.com>
2023-01-14 15:55:57 +0400

    Add files via upload

---
+++ GunGameTask/GunGameTask/Implementations/Methods.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Implementations
+{
+    internal class Methods
+    {
+        public int Ammo=30;
+
+        public void Shoot()
+        {
+            if (Ammo>0)
+            {
+                Ammo -= 1;
+                Console.WriteLine("One ammo shooted");
+            }
+            else
+            {
+                Console.WriteLine("There is no bullet left pls reload.");
+            }
+        }
+
+        public void ShootAll()
+        {
+            if (Ammo > 0)
+            {
+                Ammo = 0;
+                Console.WriteLine("All ammo have shooted.");
+                Console.WriteLine("You need to reload");
+            }
+            else
+            {
+                Console.WriteLine("There is no bullet left pls reload.");
+            }
+            
+        }
+        public void Reload()
+        {
+            Ammo = 30;
+            Console.WriteLine("Reloaded.....");
+        }
+        public void ShowBullets()
+        {
+            
+            
+               Console.WriteLine($"You have left {Ammo} bullets.");
+            
+            
+
+        }
+    }
+}
---
+++ GunGameTask/GunGameTask/Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp1.Implementations;
+
+Methods Gun = new();
+repeat:
+
+Console.WriteLine("Welcome to our Gun simulator");
+Console.WriteLine("1.Shoot one by one");
+Console.WriteLine("2.Shoot all bullets");
+Console.WriteLine("3.Reload...");
+Console.WriteLine("4.Exit from simulation.");
+Console.WriteLine("5.Show how much bullets left");
+Console.Write("Give your Order----");
+int.TryParse(Console.ReadLine(), out int Order);
+
+
+switch (Order)
+{
+	case 1:
+		Console.Clear();
+		Gun.Shoot();
+		goto repeat;
+	case 2:
+		Console.Clear();
+		Gun.ShootAll();
+		goto repeat;
+	case 3:
+		Console.Clear();
+		Gun.Reload();
+		goto repeat;
+	case 4:
+		return;
+	case 5:
+		Console.Clear();
+		Gun.ShowBullets();
+		goto repeat;
+	case 0:
+		Console.Clear();
+		Console.WriteLine("Invalid order try again...");
+		goto repeat;
+	default:
+		Console.Clear();
+		Console.WriteLine("Invalid order try again....");
+		goto repeat;
+}
